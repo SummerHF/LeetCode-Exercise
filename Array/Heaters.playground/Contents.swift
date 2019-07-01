a0ead1b40139a193de2b3674fa5b19dc574ddd99
zuwensong <zhuhaifeiswift@gmail.com>
2019-07-01 21:32:41 +0800

    Array --- Heats ----Solution

--- Array/Heaters.playground/Contents.swift
+++ Array/Heaters.playground/Contents.swift
@@ -1,53 +1,47 @@
 import UIKit
 
 /**
- Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+ Winter is coming! Your first job during the contest is to design a standard heater with fixed warm radius to warm all the houses.
  
- /// 给定一个非负整数数组A，返回一个数组，该数组由A的所有偶数元素组成，然后是A的所有奇数元素。
+ Now, you are given positions of houses and heaters on a horizontal line, find out minimum radius of heaters so that all houses could be covered by those heaters.
  
- You may return any answer array that satisfies this condition.
- 
- Example 1:
- Input: [3,1,2,4]
- Output: [2,4,3,1]
- The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+ So, your input will be the positions of houses and heaters seperately, and your expected output will be the minimum radius standard of heaters.
  
  Note:
- 1. 1 <= A.length <= 5000
- 2. 0 <= A[i] <= 5000
+ 
+ Numbers of houses and heaters you are given are non-negative and will not exceed 25000.
+ Positions of houses and heaters you are given are non-negative and will not exceed 10^9.
+ As long as a house is in the heaters' warm radius range, it can be warmed.
+ All the heaters follow your radius standard and the warm radius will the same.
+ 
+ Example 1:
+ Input: [1,2,3],[2]
+ Output: 1
+ Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
+ 
+ Example 2:
+ Input: [1,2,3,4],[1,4]
+ Output: 1
+ Explanation: The two heater was placed in the position 1 and 4. We need to use radius 1 standard, then all the houses can be warmed.
+ 
  */
 
-//print(sortArrayByParitySolutionTwo([3, 1, 2, 4]))
+/// 记录每一个房子离加热器的最小半径，然后比较，最大的即为最小半径
+/// 2 * house >= heat[i] + heat[i+1]: 这句话是最难理解的，提供了图片
 
 func findRadius(_ houses: [Int], _ heaters: [Int]) -> Int {
-    
-    var minRadius = 1
-    var tempSet = Set(heaters)
-    let housesSet = Set(houses)
-    if housesSet.isSubset(of: tempSet) {
-        return 0
-    }
-    var loop = true
-    while loop {
-        for heater in heaters {
-            let minLocation = heater - minRadius > 0 ? heater - minRadius : 0
-            let maxLocation = heater + minRadius
-            var tempArray = [Int]()
-            for i in minLocation...maxLocation {
-                tempArray.append(i)
-            }
-            tempSet.formUnion(tempArray)
+    let house = houses.sorted()
+    let heat = heaters.sorted()
+    var i = 0
+    var radius = 0
+    for house in house {
+        while i < heaters.count - 1 && 2 * house >= heat[i] + heat[i+1] {
+            i += 1
         }
-        if housesSet.isSubset(of: tempSet) {
-            print(true)
-            loop = false
-            break
-        } else {
-            minRadius += 1
-        }
+        radius = max(radius, abs(house - heat[i]))
     }
-    return minRadius
+    return radius
 }
 
-print(findRadius([10, 12, 13, 44], [1, 6, 9]))
+
 
--- README.md
+++ README.md
@@ -6,10 +6,11 @@
 
 # Array
 
-Title | Solution | Difficulty | Time | Space
----|---|---|---|---|
-[1.Sort Array By Parity](https://leetcode.com/problems/sort-array-by-parity/)| [Swift](https://github.com/SummerHF/LeetCode-Exercise/blob/master/Array/SortArrByParity.playground/Contents.swift) | Easy | O(n) | O(1)
-[2.Max Consecutive Ones](https://leetcode.com/problems/max-consecutive-ones/)| [Swift](https://github.com/SummerHF/LeetCode-Exercise/blob/master/Array/MaxConsecutive.playground/Contents.swift)| Easy| O(n) | O(1)
+Title | Solution | Difficulty | Time | Space | Ideas Of Solving Problem
+---|---|---|---|---|----|
+[1.Sort Array By Parity](https://leetcode.com/problems/sort-array-by-parity/)| [Swift](https://github.com/SummerHF/LeetCode-Exercise/blob/master/Array/SortArrByParity.playground/Contents.swift) | Easy | O(n) | O(1) | 
+[2.Max Consecutive Ones](https://leetcode.com/problems/max-consecutive-ones/)| [Swift](https://github.com/SummerHF/LeetCode-Exercise/blob/master/Array/MaxConsecutive.playground/Contents.swift)| Easy| O(n) | O(1) | 
+[3.Heaters](https://leetcode.com/problems/heaters/) | [Swift]() |Easy | O(nlogn) | O(1) | [Thinks](http://ww3.sinaimg.cn/large/006tNc79ly1g4knv9japzj30u00y4npd.jpg)
 
 
 
